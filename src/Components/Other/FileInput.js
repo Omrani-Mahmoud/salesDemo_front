a587edf2e7213ad884afbf19bd7e9b88a57c6ce0
facecheckdisnuts <omranimahmoud@gmail.com>
2020-11-11 15:53:01 +0100

    multiple images on products

--- src/Components/Other/FileInput.js
+++ src/Components/Other/FileInput.js
@@ -10,16 +10,18 @@ function FileInput() {
         setpictures([...picture]);
     }
     return (
+        <div style={{height:'190px',overflowY:'scroll'}}>
         <ImageUploader
         withIcon={true}
         buttonText='Choose images'
         onChange={onDrop}
         imgExtension={['.jpg', '.gif', '.png', '.gif']}
-        singleImage={true}
+        singleImage={false}
         withPreview={true}
         label="Your product image"
         maxFileSize={5242880}
     />
+    </div>
     )
 }
 
